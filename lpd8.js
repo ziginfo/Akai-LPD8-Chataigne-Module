767c086af807756f8124720ab7ad4e591398b831
ziginfo <34913387+ziginfo@users.noreply.github.com>
2023-10-18 06:16:03 +0200

    Add files via upload

--- lpd8.js
+++ lpd8.js
@@ -19,7 +19,7 @@ function noteOnEvent(channel, pitch, velocity, pichOffset)
 	script.log("Note on received "+channel+", "+pitch+", "+velocity);
 	offset = local.parameters.setOffset.get() ;
 	i = pitch - offset+1 ;
-    local.values.padsNote.getChild("pad" + i).set(1);
+    local.values.padsNote.getChild("pad" + i).set(velocity);
 }
 
 function noteOffEvent(channel, pitch, velocity)
@@ -27,13 +27,6 @@ function noteOffEvent(channel, pitch, velocity)
 	script.log("Note off received "+channel+", "+pitch+", "+velocity);
 	i = pitch - offset+1 ;
     local.values.padsNote.getChild("pad" + i).set(0);
-}
-
-function channelPressureEvent (channel, value)
-{
-	 script.log("Channel Pressure received "+channel+", "+value);
-	 local.values.padsProgram.program.set ("100") ;	
-	local.values.padsProgram.getChild("padPC"  + number).set(value);
 }
 
 function ccEvent(channel, number, value)
